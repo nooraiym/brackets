e3b82d71d42e9fc8846482c5b7f1f05fc91b2647
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-08-08 17:31:45 +0500

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+
+  let bracketsMap = new Map(bracketsConfig)
+  for (let char of str) {
+    const last = stack[stack.length - 1]
+    if (bracketsMap.has(char) && char !== bracketsMap.get(last)) {
+      stack.push(char)
+    } else if (last && char === bracketsMap.get(last)) {
+      stack.pop(char)
+    } else {
+      return false
+    }
+  }
+
+  return stack.length === 0
 }
